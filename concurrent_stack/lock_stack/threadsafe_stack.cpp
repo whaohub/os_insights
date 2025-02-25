22016ac2abe4555a5c92cf419b0e16341e7806a5
wanghao1 <wanghao1@maxieyetech.com>
2025-02-25 18:00:47 +0800

    update stack test

--- concurrent_stack/lock_stack/threadsafe_stack.cpp
+++ concurrent_stack/lock_stack/threadsafe_stack.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <mutex>
 #include <thread>
-#include <unordered_set>
-#include <vector>
 class Stack_overflow {};
 class Stack_underflow {};
 
@@ -17,13 +15,17 @@ class Stack {
     if (top_ == -1) {
       throw Stack_underflow();
     }
-    return data_[top_--];
+    auto value = data_[top_--];
+    std::cout << "Popped: " << value << std::endl;
+    return value;
   }
   void push(int val) {
     std::lock_guard<std::mutex> lock(mutex_);
+
     if (top_ == sz_ - 1) {
       throw Stack_overflow();
     }
+    std::cout << "Pushed: " << val << std::endl;
     data_[++top_] = val;
   }
 
@@ -34,27 +36,33 @@ class Stack {
   std::mutex mutex_;
 };
 
+void thread_push(Stack &stack) {
+  for (int i = 0; i < 10; ++i) {  // Use a small value for easier observation
+    stack.push(i);
+    std::this_thread::sleep_for(
+        std::chrono::milliseconds(10));  // Simulate delay
+  }
+}
+
+void thread_pop(Stack &stack) {
+  for (int i = 0; i < 10; ++i) {  // Use a small value for easier observation
+    try {
+      stack.pop();
+    } catch (const std::out_of_range& e) {
+      // Handle empty stack scenario
+    }
+    std::this_thread::sleep_for(
+        std::chrono::milliseconds(15));  // Simulate delay
+  }
+}
 int main() {
-  Stack s(10000);
-  for (int i = 0; i < 10000; i++) {
-    s.push(i);
-  }
+  Stack stack(10000);
 
-  std::vector<std::thread> threads;
-  std::unordered_set<int> unordered_set;
+  // Launch threads
+  std::thread t1(thread_push, std::ref(stack));
+  std::thread t2(thread_pop, std::ref(stack));
 
-  const int kThreadNum = 2;
-  for (int i = 0; i < kThreadNum; ++i) {
-    threads.emplace_back([&]() {
-      auto v = s.pop();
-      unordered_set.insert(v);
-      if (unordered_set.count(v)) {
-        std::cerr << "Error: " << v << " was poped before" << std::endl;
-      }
-    });
-  }
-
-  for (auto& t : threads) {
-    t.join();
-  }
+  // Wait for threads to complete
+  t1.join();
+  t2.join();
 }
